146dd399de2f7dd1e23977665c27d82b5e8c03e6
Sarthak <sarthakgoswamiind@gmail.com>
2024-05-06 15:50:00 +0530

    before add form and slug values in chai aur code

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -10,7 +10,7 @@ function PostCard({$id, title, featuredImage}) {
                 <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='rounded-xl' />
             </div>
             <h2
-            className='text-xl font-bold'
+            className='text-xl font-bold '
             >{title}</h2>
         </div>
     </Link>
